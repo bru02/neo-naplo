d945215ef13d344a601d088d760f4d2cd8df5e10
bru02 <bru02@freemail.hu>
2019-12-09 21:57:53 +0100

    Perf

--- frontend/src/components/dataviews/EvaluationsList.vue
+++ frontend/src/components/dataviews/EvaluationsList.vue
@@ -1,14 +1,135 @@
 <template>
   <v-container>
+    <v-tabs v-model="sortBy">
+      <v-tab key="date">
+        <v-list two-line>
+          <template v-for="(items, header) in byDate">
+            <v-list-item
+              v-for="item in items"
+              :key="item.creatingTime"
+              @click="$emit('input', item)"
+            >
+              <v-list-item-avatar>
+                {{ item.numberValue }}
+              </v-list-item-avatar>
+
+              <v-list-item-content>
+                <v-list-item-title v-text="item.subject"></v-list-item-title>
+                <v-list-item-subtitle
+                  v-text="item.theme"
+                ></v-list-item-subtitle>
+              </v-list-item-content>
+
+              <v-list-item-action>
+                {{ formatDate(item.date) }}
+              </v-list-item-action>
+            </v-list-item>
+
+            <v-divider inset :key="header + '_div'"></v-divider>
+          </template>
+        </v-list>
+      </v-tab>
+      <v-tab key="creatingTime">
+        <v-list two-line>
+          <template v-for="(items, header) in byCreatingTime">
+            <v-list-item
+              v-for="item in items"
+              :key="item.creatingTime"
+              @click="$emit('input', item)"
+            >
+              <v-list-item-avatar>
+                {{ item.numberValue }}
+              </v-list-item-avatar>
+
+              <v-list-item-content>
+                <v-list-item-title v-text="item.subject"></v-list-item-title>
+                <v-list-item-subtitle
+                  v-text="item.theme"
+                ></v-list-item-subtitle>
+              </v-list-item-content>
+
+              <v-list-item-action>
+                {{ formatDate(item.date) }}
+              </v-list-item-action>
+            </v-list-item>
+
+            <v-divider inset :key="header + '_div'"></v-divider>
+          </template>
+        </v-list>
+      </v-tab>
+      <v-tab key="subject">
+        <v-list two-line subheader>
+          <template v-for="(items, header) in groupedEvaluations">
+            <v-subheader inset :key="header"
+              >{{ header }} &mdash; &#10240;
+              <span :class="[`${getEvaluationColor(getAverage(items))}--text`]">
+                {{ getAverage(items) }}</span
+              >
+            </v-subheader>
+            <v-list-item
+              v-for="item in items"
+              :key="item.creatingTime"
+              @click="$emit('input', item)"
+            >
+              <v-list-item-avatar>
+                {{ item.numberValue }}
+              </v-list-item-avatar>
+
+              <v-list-item-content>
+                <v-list-item-title v-text="item.subject"></v-list-item-title>
+                <v-list-item-subtitle
+                  v-text="item.theme"
+                ></v-list-item-subtitle>
+              </v-list-item-content>
+
+              <v-list-item-action>
+                {{ formatDate(item.date) }}
+              </v-list-item-action>
+            </v-list-item>
+
+            <v-divider inset :key="header + '_div'"></v-divider>
+          </template>
+        </v-list>
+      </v-tab>
+      <v-tab key="value">
+        <v-list two-line subheader>
+          <template v-for="(items, header) in byValue">
+            <v-subheader inset :key="header">{{ header }} </v-subheader>
+            <v-list-item
+              v-for="item in items"
+              :key="item.creatingTime"
+              @click="$emit('input', item)"
+            >
+              <v-list-item-avatar>
+                {{ item.numberValue }}
+              </v-list-item-avatar>
+
+              <v-list-item-content>
+                <v-list-item-title v-text="item.subject"></v-list-item-title>
+                <v-list-item-subtitle
+                  v-text="item.theme"
+                ></v-list-item-subtitle>
+              </v-list-item-content>
+
+              <v-list-item-action>
+                {{ formatDate(item.date) }}
+              </v-list-item-action>
+            </v-list-item>
+
+            <v-divider inset :key="header + '_div'"></v-divider>
+          </template>
+        </v-list>
+      </v-tab>
+    </v-tabs>
     <v-list two-line subheader>
       <template v-for="(items, header) in sorted">
         <v-subheader inset :key="header" v-if="!!header"
           >{{ header }}
           <template v-show="sortBy == 'subject'">
-            &mdash;
-            <span :class="[`${getEvaluationColor(getAverage(items))}--text`]">{{
-              getAverage(items)
-            }}</span>
+            &mdash; &#10240;
+            <span :class="[`${getEvaluationColor(getAverage(items))}--text`]">
+              {{ getAverage(items) }}</span
+            >
           </template>
         </v-subheader>
         <v-list-item
