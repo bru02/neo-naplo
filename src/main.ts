13ef9a3b9c15586a281f27b802289f385b283881
bru02 <bru02@freemail.hu>
2020-02-21 15:07:26 +0100

    feat analytics & perf monitoring

--- src/main.ts
+++ src/main.ts
@@ -93,6 +93,27 @@ if (
   });
 }
 
+import * as firebase from 'firebase/app';
+import 'firebase/analytics';
+import 'firebase/performance';
+
+if (process.env.NODE_ENV === 'production') {
+  const firebaseConfig = {
+    apiKey: 'AIzaSyBS6oHXLZJJbCeVHTGoX15JhNI_tMuT7Hk',
+    authDomain: 'efilc-239813.firebaseapp.com',
+    databaseURL: 'https://efilc-239813.firebaseio.com',
+    projectId: 'efilc-239813',
+    storageBucket: 'efilc-239813.appspot.com',
+    messagingSenderId: '189077601555',
+    appId: '1:189077601555:web:318300dfc3a2d300',
+    measurementId: 'G-YMDKBPFV5P'
+  };
+  firebase.initializeApp(firebaseConfig);
+
+  const perf = firebase.performance();
+  const analytics = firebase.analytics();
+}
+
 import * as helpers from './helpers';
 for (const [name, helper] of Object.entries(helpers)) {
   Vue.filter(name, helper);
